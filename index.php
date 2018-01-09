57bc27fc38599378dd7f2ebeed343c70b7cc921c
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-09 13:14:02 -0700

    Added states list

--- index.php
+++ index.php
@@ -11,9 +11,27 @@
 		<h1><b>About Jeff</b></h1>
 		<img src="htmlstud.jpg" alt="gettin ladies">
 		<h1>This guy gets it!!!!</h1>
-		<p>Hello, my name is Jeffrey Brink, and I am a student in the Deep Dive Coding Academy.</p>
-		<p>I hope to be a front end web developer, where I can put my creativity to work.</p>
+
+
+		<div><p>Hello, my name is Jeffrey Brink, and I am a student in the Deep Dive Coding Academy.</p>
+		</div>
+
+		<div><p>I hope to be a front end web developer, where I can put my creativity to work.</p>
+		</div>
 		<p>While exploring development tools I found these logos and thought I would add them.</p>
+		<p></p>
 		<p>Margins, spacing, padding and such are still a mystery to me so it's a little off. </p>
 		<img src="gitkraken.jpg" alt="Kraken">
+
+		<div>
+			<p>
+			I have lived in 6 different states:
+			<ul>
+				New Mexico
+				<br>Colorado
+				<br>California
+				<br>Florida
+				<br>Missouri
+				North Carolina
+				</ul>
 </html>
