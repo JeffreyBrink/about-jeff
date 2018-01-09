98a478aa1b39a45bd83b68cacbb241cc9f66d217
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-09 10:30:05 -0700

    added .css stylesheet

--- index.php
+++ index.php
@@ -9,6 +9,4 @@
 	</head>
 	<body>
 		<h1><b>About Jeff</b></h1>
-		<img src="http://www.quickmeme.com/img/8d/8d275361db75d3f0701e389fbfd6598a031f17559f1c671ba8e353f296e815bf.jpg" alt="Gettin the ladies">
-	</body>
 </html>
