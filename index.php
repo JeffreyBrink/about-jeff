58b1e8545ccb86a72b6a0d38c4c4a7562b9a0719
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-09 12:52:56 -0700

    added background image

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 		<h1><b>About Jeff</b></h1>
 		<img src="htmlstud.jpg" alt="gettin ladies">
 		<h1>This guy gets it!!!!</h1>
-		<p>My name is Jeffrey Brink, and I am a student in the Deep Dive Coding Academy.</p>
+		<p>Hello, my name is Jeffrey Brink, and I am a student in the Deep Dive Coding Academy.</p>
 		<p>I hope to be a front end web developer, where I can put my creativity to work.</p>
 		<p>While exploring development tools I found these logos and thought I would add them.</p>
 		<p>Margins, spacing, padding and such are still a mystery to me so it's a little off. </p>
