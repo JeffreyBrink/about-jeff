89bd2c6b44dd43fbfab960a0da3e1279dea0e156
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-09 11:37:11 -0700

    added wife image and comment paragraph

--- index.php
+++ index.php
@@ -10,4 +10,6 @@
 	<body>
 		<h1><b>About Jeff</b></h1>
 		<img src="htmlstud.jpg" alt=" gettin ladies">
+		<p>Although His Wife Would Tend to Disagree</p>
+		<img src="jeffMadWife.jpg" alt="angry wife">
 </html>
