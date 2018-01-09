c451374052c257316c8b1d56113ace50e8a6f8c7
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-09 10:00:47 -0700

    added image

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
 	</head>
 	<body>
 		<h1>About Jeff</h1>
+		<img src="http://www.quickmeme.com/img/8d/8d275361db75d3f0701e389fbfd6598a031f17559f1c671ba8e353f296e815bf.jpg" alt="Gettin the ladies">
 	</body>
 </html>
